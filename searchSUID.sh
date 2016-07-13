4de54e9904ad9a032122415fd4a5ee3b69ff5996
joao-lucas <joaolucas@linuxmail.org>
2016-07-12 21:33:11 -0300

    commit

--- searchSUID.sh
+++ searchSUID.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Script para procurar arquivos com a permissão SUID ativo.
-# Author: Joao Lucas <joaolucas@linuxmail.org>
-# Data: 2016-07-12
-#
+# Scrip para procurar arquivos com a permissão SUID ativo.
+# Data: 20160528
+# Autor: Joao Lucas <joaolucas@linuxmail.org>
 
-find / -path '/proc' -prune -or -perm -u+s -exec ls -l {} \; | tee suids.txt
+find / -path '/proc' -prune -or -perm -u+s -exec ls -ld {} \; | tee suids.txt
