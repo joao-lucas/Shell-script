92accc49896514a25179ed52e0fd80891fa1e781
Joao Lucas <joaolucas@linuxmail.org>
2016-08-01 14:41:30 -0300

    Script para resolver problemas com chaves GPG quando adicionado novas listas de repositos em /etc/apt/sources.list

---
+++ adicionar_key_gpg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Sempre que adicionar novos repositorios no arquivo /etc/apt/sources.list, você começará a receber um erro de assinaturas GPG, este script foi criado para resolver este problema. 
+# Data: 01/08/2016
+# Autor: João Lucas <joaolucas@linuxmail.org>
+#
+
+if [ -z "$1" ]
+then
+        echo "Especifique a chave a ser adicionada"
+        echo "USO: $0 <CHAVE_GPG>"
+        exit 2
+fi
+
+gpg --keyserver pgpkeys.mit.edu --recv-key $1
+gpg --export --armor $1 | sudo apt-key add -
+exit 0
