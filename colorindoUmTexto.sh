1f40c4b01e1e245b9238721f3e3b3a345c24621d
João Lucas <joaolucas@linuxmail.org>
2016-08-02 00:57:57 -0300

    Delete colorindoUmTexto.sh

--- colorindoUmTexto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 033 é o codigo octal do Esc
-# 31 representa a cor vermelha
-# 40 representa o fundo preto
-# 1 serve para colocar o atributo brilhante
-
-echo -e '\033[31;40;1m Joao Lucas \033[m'
