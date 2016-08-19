efb96e0af4399e6dc45b08ea5a46d5a77cd1f37b
Joao Lucas <joaolucas@linuxmail.org>
2016-08-19 00:08:34 -0300

    Alguns exemplos de uso do firewall iptables

--- regras_iptables1.sh
+++ regras_iptables1.sh
@@ -80,3 +80,75 @@ iptables -A FORWARD -p tcp -i eth0 --dport 80 -j ACCEPT
 iptables -A FORWARD -p tcp -i eth0 --dport 443 -j ACCEPT
 iptables -A FORWARD -p tcp -i eth0 -j LOG
 iptables -A FORWARD -p tcp -i eth0 -j REJECT
+# A regra "iptables -A FORWARD -j LOG" é uma boa opção  durante a fase de teste, pois ela faz com que o iptables logue todos os pacotes que forem encaminhados, tanto no envio, quanto na resposta, permitindo que você verifique o que esta ocorrendo quando algo nao estiver funcionando. Você pode acompanhar o log usando o comando dmesg
+
+# Bloqueia o acesso á web a partir de um determinado IP
+iptables -A FORWARD -p tcp -s 192.168.1.67 -j REJECT
+# Esta regra deve ir logo no inicio do scritp, antes das regras que abrem as portas de saída, caso contrário não surtirá efeito.
+# Lembre-se de que o iptables processa as regras sequencialmente: se uma compartilhha a conexão com todos micros da rede, não adianta tentar bloquear para determinados endereços depois. As regras com as excessões devem sempre vir antes da regra mais geral
+
+
+#
+## Bloqueando domínios
+# É possivel bloquear ou permitir com vbase no dominio, tanto para entrada quanto saída. Isso permite bloquear sites e programas diretamente a partir do firewall, sem precisar instalar um servidor squid e configura-lo como proxy transparente.
+# Neste caso, usamos o parâmetro "-d"(destiny) do iptables, seguido do dominio desejado.
+# Para bloquear o facebook
+iptables -A OUTPUT -d www.facebook.com -j REJECT
+iptables -A FORWARD -d www.facebook.com -j REJECT
+#  A primeira linha bloqueia pacotes de saída destinados ao dominio, ou seja, impede que ele seja acessado a partir da propria maquina local. A segunda linha bloqueia o forward de pacotes destinados a ele(dominio), ou seja, impede que outras maquinas de rede local, que acessem atraves de uma conexão compartilhada, acessem o dominio.
+
+ # Se voce for paranoico, você pode usar tambem a regra:
+ iptables -A INPUT -s www.facebook.com -j DROP
+ # Esta regra impede também que qualquer pacote proviniente de facebook.com chague até sua maquina.
+
+# Assim como no caso do aquid, ao dicidir bloquear o acesso a sites ou endereços diversos utilizando o iptables, voce vai logo acabar com uma lista relativamente grande de endereços a bloquear. Diferente do aquid, o iptables nao oferece uma opção para carregar a lista dos endereços que devem ser bloqueados a partir de um arquivo texto, mas isso pode ser resolvido com um script simples, que leia o conteúdo do arquivo e gere a regras correspondentes.
+# Comece criando um arquivo testo contendo os dominios ou endereços IP que deseja bloquear, um por linha:
+# >  www.facebook.com
+# >  www.xvideos.com
+# >  www.orkut.com
+# Em seguida, adicione esta função no inicio do seu script de firewall, especificando o arquivo de texto com os endereços:
+for end in 'cat /etc/sites_bloqueados'
+do
+        iptables -A OUTPUT -d $end -j REJECT
+        iptables -A FORWARD -d $end -j REJECT
+done
+
+#
+## Regras adicionais de segurança
+# Em um servidor dedicado, não faz muito sentido bloquear a respostas a pings, já que de qualquer forma, ele precisará ficar com um conjunto de portas abertas. Ao invés de bloquear os pings, que afinal podem ser úteis em algumas situações, você pode limitar as respostas a apenas uma por segundo, evitano que alguém de má fé possa enviar um grande volume de pings (como o comando ping -f), como parte de um ataque DoS.
+# Nesse caso seria
+iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
+
+# Se seu servidor não irá atuar com um roteador, é prudente desativar o suporte a ICMP redirects. Este é um recurso utiizado por roteadores para alertar outros de que existe um melhor caminho para um determinado endereço ou rede, mas não tem uso legitimo em um servidor que não roteia pacotes:
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
+
+# Outra configuração desejada é desativar o suporte a ping broadcasts, um recurso que tem poucos usos legitimos e pode ser usado para fazer com que servidores participem involuntariamente de ataques DoS, enviando grandes quantidades de pings a outros servidores dentreo da mesma faixa de endereços. Ele já vem desativado em quase todas as distruibuições atuais, mas não custa verificar:
+echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+
+# Mais uma opção importante manter desativada é o suporte ao souce routing. Este é um recurso usado para testes de roteadores, que permite ao emissor especificar qual o caminho que o pacote tomará até o destino e também o caminho de volta. Ele é perigoso, pois permite falsear pacotes, fazendo com que eles pareçam vir de outro endereço e, ao mesmo tempo, fazeer com que as respostas realmente sejam recebidas, permitindo abrir a conexão e tranferir dados. Em outras palavra, se voce inclui regras que permitem o acesso de determiandos endereços e esqueceu o suporte source routing ativo, um atacante que soubesse quais são os endereços autorizados poderia abrir conexões com o seu servidor se fazendo passar por um deles, um risco que você cm certeza gostaria de evitar. COmo o recurso não possui outros usos legitimos, é fortemente recomendável que você o mantenha desativado:
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route
+
+# Diferente do servidor de rede local que compartilha a conexão, o servidor não deverá compartilhar a conexão, nem encaminhar pacotes de outros hosts. Vocẽ pode deixar explicito desativando o suporte a ip_forward:
+echo 0 > /proc/sys/net/ipv4/ip_forward
+
+# Concluindo, temos o suporte a SYN cookies, Um dos tipos mais comuns de ataques DoS e também um dos mais efetivos é o SYN Flood. Este tipo de ataque consiste em enviar um grande volume de pacotes SYN até o alvo, sem nunca efetivamente abrir a conexão. Como os pacotes SYN possuem alguns poucos bytes, o ataque pode ser feito mesmo a partir de uma conexão domestica.
+# Como existe um limite de conexões TCP que o servudor pode manter ativas simultaneamente, um grande volume de pacotes SYN podem estourar o limite de conexões, fazendo com que o servidor deixe de responder a novas conexões, mesmo que exista banda disponivel.
+# No linux, isso pode ser evitado de forma bastante simples, ativando o iso de SYN Cookies, um recurso oferecido diretamente pelo Kernel, o que é feito com o comando abaixo, que pode incluido no seu script de firewall:
+echo 1 > /proc/sys/net/ipv4/ip_syncookies
+# Ao atuvas o recurso, o sistema passa a responder ao pacote SYN inicial com um cookie, que identifica o cliente. Com isso o sistema aloca um espaço para a conexão apenas após receber o pacote ACK de resposta, tornando o ataque inefetivo. O atacante ainda pode consumir um pouco de banda, obrigando o servidor a enviar um grande volume de SYN Cookie de resposta, mas o efeito sobre o servidor é minimo
+
+# Concluindo, adicione também as regras que ativam o uso de rp_filter, de forma que o firewall sempre responda aos pacotes na mesma interface da qual eles foram originados, o que previne ataques diversos que tentem tirar proveito da regra que permite conexões na interface de loopback. Outra opção interessante é o blouqeio de pacotes inválifos, o que também melhora a segurança contra ataques diversos, incluindo pacotes enviados sem serem precedidos pelo envio do pacote SYN e da abertura de conexão:
+echo 1 > /proc/sys/net/ipv4/conf/default/rp_filter
+iptables -A INPUT -m state --state INVALID -j DROP
+
+# Juntando tudo, teriamos:
+iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
+echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route
+echo 0 > /proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv4/tcp_syncookies
+echo 1 > /proc/sys/net/ipv4/conf/default/rp_filter
+iptables -A INPUT -m state --state INVALID -j DROP
+# Essas regras devemser adicionadas logo no inicio do script, de forma que sejam carregadas antes de qualquer regra que abra portas ou permita o acesso de endereços ou faixas de enredereços.
+
